0912e36ada4515933214d1e2512fd1672e01e681
MegaScience <megascientifical@yahoo.com>
2016-06-19 21:10:50 -0400

    Putting what I've got
    I can't for the life of me figure out why I can't access the "nexon" object provided in the actual page. It provides useful functionality my code could use for properly naming pages.

--- Change_Page_Titles_Properly.user.js
+++ Change_Page_Titles_Properly.user.js
@@ -6,27 +6,69 @@
 // @exclude     /https?:\/\/forum2\.nexon\.net\/.*?/
 // @run-at      document-idle
 // @version     1
-// @grant       none
+// @grant       GM_xmlhttpRequest
 // ==/UserScript==
 
 function setTitle() {
-	var hash = window.location.hash,
-		title = hash.match(/.*?-([^%]+)(%|$)/);
-	title = (title == null) ? "" : title[1].replace(/-/g, " ") + " - "; 
-	document.title = title + siteName;
+	//var hash = window.location.hash,
+	//	title = hash.match(/.*?-([^%]+)(%|$)/);
+	var hash = window.location.hash.match(/%2F(.*?)-/),
+		title = hash === null ? "" : "%2F" + hash[1];
+	GM_xmlhttpRequest({
+	  method: "GET",
+	  url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fforum2.nexon.net" + title + "%22%20and%20xpath%3D'%2F%2Fhead%2Ftitle'&format=json",
+	  onload: function(response) {
+		var title = "",
+			resObj = JSON.parse(response.responseText);
+		if(resObj.query.results !== null && resObj.query.results.title !== "Nexon Forums")
+			title = JSON.parse(response.responseText).query.results.title.replace(" - Nexon Forums", "") + " - ";
+		document.title = title + siteName;
+	  }
+	});
+	//title = (title == null) ? "" : title[1].replace(/-/g, " ") + " - "; 
+	//document.title = title + siteName;
 }
 
-function getMetaContentByName(name, content) {
-	content = (typeof content === 'undefined') ? 'content' : content;
-	var element = document.querySelector("metameta[property='" + name + "'], meta[name='" + name + "']");
-	return (element == null) ? document.title : element.getAttribute(content).replace(" - ", " Forum - ");
+function getSiteName() {
+	try {
+		//alert(nexon.util.getGame());
+		//console.log(nexon);
+		//document.querySelector(".header-logo img[alt], .logo img[alt]").getAttribute("alt");
+		var sn = " ";
+		for(var i = 0, len = nexon.games.gameList.length, domain = window.location.host; i < len; i++) {
+			if(nexon.games.gameList[i].domainRegex && nexon.games.gameList[i].domainRegex.test(domain)) {
+				sn += nexon.games.gameList[i].name;
+				break;
+			}
+		}
+		return sn + " Forum";
+	}
+	catch(e) {
+		//alert(e.message);
+		return "Nexon Forums";
+	}
 }
 
-var siteName = getMetaContentByName("og:site_name");
+var siteName = getSiteName();
 
 setTitle();
 
 window.addEventListener("hashchange", setTitle, false);
+
+//document.querySelector(".component.component-breadcrumbs span").onclick = function () {console.log(window);};
+
+//console.log(confirmObject(["nexon", "games", "gameList"]));
+function confirmObject(array) {
+	var tempObject = window;
+	for(var i = 0, len = array.length; i < len; i++) {
+		if(tempObject[array[i]] === undefined) {
+			alert("Could not locate object property: " + array.slice(0, (i + 1)).join("."));
+			return false;
+		}
+		tempObject = tempObject[array[i]];
+	}
+	return tempObject;
+}
 
 /*
 function init() {
