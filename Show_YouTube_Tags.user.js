aab4e8dd0247436c732d63585e03b1ed68498b86
MegaScience <megascientifical@yahoo.com>
2016-07-02 11:41:07 -0400

    Functions > Object
    Because it felt cleaner than having so many functions floating individually.

--- Show_YouTube_Tags.user.js
+++ Show_YouTube_Tags.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name        Show Youtube Tags
+// @name        Show Youtube Video Tags
 // @namespace   https://www.youtube.com/
 // @description Adds tags for the current video to the video description, as the website previously offered. Currently, tags are buried in the page code.
 // @match       *://*.youtube.com/*
 // @noframes
-// @version     2.0
+// @version     3.0
+// @grant       none
 // ==/UserScript==
 
-console.log("Show YouTube Tags: Loaded.");
-var information = {
+var showYTT = {
 	debug: false,
 	echoTypes: ["", "Error - ", "Warning - "],
 	messages: {
 		loaded: {string: "Loaded.", type: 0, debug: false},
-		error: {string: "Tag addition canceled due to error(s).", type: 2, debug: true},
+		error: {string: "Tag addition will cancel due to error(s).", type: 2, debug: true},
 		added: {string: "Tags added.", type: 0, debug: false},
 		wrongPage: {string: "Non-video Page: ", type: 2, debug: true},
 		iFrame: {string: "Script running in incorrect scope.", type: 2, debug: false},
@@ -24,92 +24,87 @@ var information = {
 		returning: {string: "Sending required data.", type: 0, debug: true},
 		objProp: {string: "Could not locate object property: ", type: 1, debug: false},
 		iFrameOut: {string: "Only applying listeners to top-level scope.", type: 2, debug: true}
+	},
+	addTags: function () {
+		var data = this.errorCheck();
+		if(data.errState) return;
+		var tags = data.keywords.replace(/,/g, ", ") || "-",
+			li = data.meta.cloneNode(true);
+		li.id = "showYouTubeTags";
+		li.getElementsByTagName("h4")[0].innerHTML = " Tags ";
+		li.getElementsByTagName("li")[0].innerHTML = tags;
+		data.container.appendChild(li);
+		this.tagLog("added");
+	},
+	errorCheck: function () {
+		var data = { errState: false };
+		try {
+			if(location.pathname !== "/watch")
+				throw ["wrongPage", location.pathname];
+			if(this.isFrame())
+				throw "iFrame";
+			if(document.getElementById("showYouTubeTags"))
+				throw "exists";
+			data.keywords = this.confirmObject(["ytplayer", "config", "args", "keywords"]);
+			if(typeof data.keywords === "boolean")
+				throw false;
+			data.container = document.getElementsByClassName("watch-extras-section")[0];
+			if(data.container === null)
+				throw "descMiss";
+			data.meta = data.container.lastElementChild;
+			if(data.meta.getElementsByTagName("h4").length === 0 || data.meta.getElementsByTagName("li").length === 0)
+				throw "descChan";
+		}
+		catch(e) {
+			if(Array.isArray(e))
+				this.tagLog(e[0], e[1]);
+			else if(e !== false)
+				this.tagLog(e);
+			data.errState = true;
+			this.tagLog("error");
+		}
+		finally {
+			this.tagLog("returning");
+			return data;
+		}
+	},
+	confirmObject: function (array) {
+		var tempObject = window;
+		for(var i = 0, len = array.length; i < len; i++) {
+			if(tempObject[array[i]] === undefined) {
+				this.tagLog("objProp", array.slice(0, (i + 1)).join("."));
+				return false;
+			}
+			tempObject = tempObject[array[i]];
+		}
+		return tempObject;
+	},
+	tagLog: function (message, append) {
+		var data = (typeof message === "string" ? this.messages[message] : message);
+		if(data.debug && !this.debug) return;
+		var string = "Show YouTube Tags" + (data.debug ? " [Debug]" : "") + ": " + this.echoTypes[data.type] + data.string + (typeof append === "undefined" ? "" : append);
+		switch(data.method || 1) {
+			case 2:
+				alert(string);
+				return true;
+			//case 1:
+			default:
+				console.log(string);
+				return true;
+		}
+		return false;
+	},
+	isFrame: function () {
+		return window.frameElement || window.top !== window.self;
 	}
 };
 
-function appendTags() {
-	var data = errorCheckTags();
-	if(data.errState) {
-		tagLog("error");
-		return;
-	}
-	var tags = data.keywords.replace(/,/g, ", ") || "-",
-		li = data.meta.cloneNode(true);
-	li.id = "showYouTubeTags";
-	li.getElementsByTagName("h4")[0].innerHTML = " Tags ";
-	li.getElementsByTagName("li")[0].innerHTML = tags;
-	data.container.appendChild(li);
-	tagLog("added");
-}
+showYTT.tagLog("loaded");
 
-function errorCheckTags() {
-	var data = { errState: false };
-	try {
-		if(location.pathname !== "/watch")
-			throw ["wrongPage", location.pathname];
-		if(isFrame())
-			throw "iFrame";
-		if(document.getElementById("showYouTubeTags"))
-			throw "exists";
-		data.keywords = confirmObject(["ytplayer", "config", "args", "keywords"]);
-		if(typeof data.keywords === "boolean")
-			throw false;
-		data.container = document.getElementsByClassName("watch-extras-section")[0];
-		if(data.container === null)
-			throw "descMiss";
-		data.meta = data.container.lastElementChild;
-		if(data.meta.getElementsByTagName("h4") === 0 || data.meta.getElementsByTagName("li") === 0)
-			throw "descChan";
-	}
-	catch(e) {
-		if(Array.isArray(e))
-			tagLog(e[0], e[1]);
-		else if(e !== false)
-			tagLog(e);
-		data.errState = true;
-	}
-	finally {
-		tagLog("returning");
-		return data;
-	}
-}
-
-function confirmObject(array) {
-	var tempObject = window;
-	for(var i = 0, len = array.length; i < len; i++) {
-		if(tempObject[array[i]] === undefined) {
-			tagLog("objProp", array.slice(0, (i + 1)).join("."));
-			return false;
-		}
-		tempObject = tempObject[array[i]];
-	}
-	return tempObject;
-}
-
-function tagLog(message, append) {
-	var data = (typeof message === "string" ? information.messages[message] : message);
-	if(data.debug && !information.debug) return;
-	var string = "Show YouTube Tags" + (data.debug ? " [Debug]" : "") + ": " + information.echoTypes[data.type] + data.string + (typeof append === "undefined" ? "" : append);
-	switch(data.method || 1) {
-		case 2:
-			alert(string);
-			return true;
-		//case 1:
-		default:
-			console.log(string);
-			return true;
-	}
-	return false;
-}
-
-function isFrame() {
-	return window.frameElement || window.top !== window.self;
-}
-
-if(!isFrame()) {
-	window.addEventListener("readystatechange", appendTags, true);
-	window.addEventListener("spfdone", appendTags);
+if(!showYTT.isFrame()) {
+	window.addEventListener("readystatechange", function () { showYTT.addTags(); }, true);
+	window.addEventListener("spfdone", function () { showYTT.addTags(); });
 }
 else {
-	tagLog("iFrameOut");
+	showYTT.tagLog("iFrameOut");
 }
