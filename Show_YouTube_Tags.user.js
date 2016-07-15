7a45a9f550bf34255227f16ad96a32efaf84061c
MegaScience <megascientifical@yahoo.com>
2016-07-15 09:51:35 -0400

    Changing tagLog usage and function

--- Show_YouTube_Tags.user.js
+++ Show_YouTube_Tags.user.js
@@ -10,20 +10,22 @@
 
 var showYTT = {
 	debug: false,
-	echoTypes: ["", "Error - ", "Warning - "],
+	echoType: ["log", "error", "warn"],
+	logString: "Show YouTube Tags%s: %s%s",
 	messages: {
-		loaded: {string: "Loaded.", type: 0, debug: false},
-		error: {string: "Tag addition will cancel due to error(s).", type: 2, debug: true},
-		added: {string: "Tags added.", type: 0, debug: false},
-		wrongPage: {string: "Non-video Page: ", type: 2, debug: true},
-		iFrame: {string: "Script running in incorrect scope.", type: 2, debug: false},
-		exists: {string: "Tags already added. Avoiding adding taglist twice.", type: 2, debug: false},
-		//objMiss: {string: "Object unable to be located.", type: 1, debug: true},
-		descMiss: {string: "Could not locate required description area.", type: 1, debug: false},
-		descChan: {string: "Description area format changed.", type: 1, debug: false},
-		returning: {string: "Sending required data.", type: 0, debug: true},
-		objProp: {string: "Could not locate object property: ", type: 1, debug: false},
-		iFrameOut: {string: "Only applying listeners to top-level scope.", type: 2, debug: true}
+		loaded: {type: 0, debug: false, string: "Loaded."},
+		error: {type: 2, debug: true, string: "Tag addition will cancel due to error(s)."},
+		added: {type: 0, debug: false, string: "Tags added. "},
+		wrongPage: {type: 2, debug: true, string: "Non-video Page: "},
+		iFrame: {type: 2, debug: false, string: "Script running in incorrect scope."},
+		exists: {type: 2, debug: false, string: "Tags already added. Avoiding adding taglist twice."},
+		//objMiss: {type: 1, debug: true, string: "Object unable to be located."},
+		descMiss: {type: 1, debug: false, string: "Could not locate required description area."},
+		descChan: {type: 1, debug: false, string: "Description area format changed."},
+		returning: {type: 0, debug: true, string: "Sending required data."},
+		objProp: {type: 1, debug: false, string: "Could not locate object property: "},
+		iFrameOut: {type: 2, debug: true, string: "Only applying listeners to top-level scope."},
+		unknownIssue: {type: 2, debug: true, string: "Unaccounted issue: "}
 	},
 	addTags: function () {
 		var data = this.errorCheck();
@@ -34,45 +36,55 @@ var showYTT = {
 		li.getElementsByTagName("h4")[0].innerHTML = " Tags ";
 		li.getElementsByTagName("li")[0].innerHTML = tags;
 		data.container.appendChild(li);
-		this.tagLog("added");
+		this.tagLog(this.messages.added, "(" + data.video_id + ")");
+		if(!this.debug) return;
+		this.tagLog({type: 0, debug: false, string: "Testing..."});
+		this.tagLog(this.messages.error);
+		this.tagLog(this.messages.descMiss);
+		this.tagLog(this.messages.returning);
 	},
 	errorCheck: function () {
 		var data = { errState: false };
 		try {
 			if(location.pathname !== "/watch")
-				throw ["wrongPage", location.pathname];
+				throw [this.messages.wrongPage, location.pathname];
 			if(this.isFrame())
-				throw "iFrame";
+				throw this.messages.iFrame;
 			if(document.getElementById("showYouTubeTags"))
-				throw "exists";
+				throw this.messages.exists;
 			data.keywords = this.confirmObject(["ytplayer", "config", "args", "keywords"]);
 			if(typeof data.keywords === "boolean")
 				throw false;
 			data.container = document.getElementsByClassName("watch-extras-section")[0];
 			if(data.container === null)
-				throw "descMiss";
+				throw this.messages.descMiss;
 			data.meta = data.container.lastElementChild;
 			if(data.meta.getElementsByTagName("h4").length === 0 || data.meta.getElementsByTagName("li").length === 0)
-				throw "descChan";
+				throw this.messages.descChan;
+			data.video_id = this.confirmObject(["ytplayer", "config", "args", "video_id"]);
+			if(typeof data.video_id === "boolean")
+				data.video_id = "ID Not Found";
 		}
 		catch(e) {
-			if(Array.isArray(e))
+			data.errState = true;
+			if(typeof e.message !== "undefined")
+				this.tagLog(this.messages.unknownIssue, e.message);
+			else if(Array.isArray(e))
 				this.tagLog(e[0], e[1]);
 			else if(e !== false)
 				this.tagLog(e);
-			data.errState = true;
-			this.tagLog("error");
+			this.tagLog(this.messages.error);
 		}
 		finally {
-			this.tagLog("returning");
+			this.tagLog(this.messages.returning);
 			return data;
 		}
 	},
 	confirmObject: function (array) {
 		var tempObject = window;
 		for(var i = 0, len = array.length; i < len; i++) {
-			if(tempObject[array[i]] === undefined) {
-				this.tagLog("objProp", array.slice(0, (i + 1)).join("."));
+			if(typeof tempObject[array[i]] === "undefined") {
+				this.tagLog(this.messages.objProp, array.slice(0, (i + 1)).join("."));
 				return false;
 			}
 			tempObject = tempObject[array[i]];
@@ -81,30 +93,21 @@ var showYTT = {
 	},
 	tagLog: function (message, append) {
 		var data = (typeof message === "string" ? this.messages[message] : message);
-		if(data.debug && !this.debug) return;
-		var string = "Show YouTube Tags" + (data.debug ? " [Debug]" : "") + ": " + this.echoTypes[data.type] + data.string + (typeof append === "undefined" ? "" : append);
-		switch(data.method || 1) {
-			case 2:
-				alert(string);
-				return true;
-			//case 1:
-			default:
-				console.log(string);
-				return true;
-		}
-		return false;
+		if(data.debug && !this.debug) return false;
+		console[this.echoType[data.type]](this.logString, (data.debug ? " [Debug]" : ""), data.string, (typeof append === "undefined" ? "" : append));
+		return true;
 	},
 	isFrame: function () {
 		return window.frameElement || window.top !== window.self;
 	}
 };
 
-showYTT.tagLog("loaded");
+showYTT.tagLog(showYTT.messages.loaded);
 
 if(!showYTT.isFrame()) {
 	window.addEventListener("readystatechange", function () { showYTT.addTags(); }, true);
 	window.addEventListener("spfdone", function () { showYTT.addTags(); });
 }
 else {
-	showYTT.tagLog("iFrameOut");
+	showYTT.tagLog(this.messages.iFrameOut);
 }
