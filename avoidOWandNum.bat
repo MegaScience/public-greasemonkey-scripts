f0495f08300caa60b3578d1a86b8782137bf2305
MegaScience <megascientifical@yahoo.com>
2017-01-10 10:52:17 -0500

    Changing reserved name

--- avoidOWandNum.bat
+++ avoidOWandNum.bat
@@ -1,18 +1,18 @@
 @echo off
 :: Based on: http://stackoverflow.com/a/17388270/5857393
-:: Use: getProperName "%toFolder%" "%frFolder%" "%fName%" "%fName%"
+:: Use: getProperName "%toFolder%" "%frFolder%" "%fileName%" "%fileName%"
 setlocal
 set toFolder=%~1
 set frFolder=%~2
-set fName=%~3
-set fType=%~4
-set "file=%fName%"
+set fileName=%~3
+set fileType=%~4
+set "file=%fileName%"
 cd ..\%toFolder%
-if not exist "%file%.%fType%" goto result
+if not exist "%file%.%fileType%" goto result
 :loop
 set /a fCounter+=1
-set "file=%fName%-%fcounter%"
-if exist "%file%.%fType%" goto loop
+set "file=%fileName%-%fCounter%"
+if exist "%file%.%filType%" goto loop
 :result
 cd ..\%frFolder%
 echo %file%
