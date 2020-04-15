49c5066130caa234885a9e63b3799137fa9a7087
MegaScience <megascientifical@yahoo.com>
2020-04-14 22:27:15 -0400

    Update Channelate_Extra_Panel_on_Same_Page.user.js

--- Channelate_Extra_Panel_on_Same_Page.user.js
+++ Channelate_Extra_Panel_on_Same_Page.user.js
@@ -4,41 +4,20 @@
 // @description Adds the Extra Panel for each comic back to the comic page.
 // @include     /^https?:\/\/(?:www\.)?channelate\.com\/(?:(?:comic|\d{4}\/\d{2}\/\d{2})\/[\w-]+\/?)?$/
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @version     2.0
+// @version     2.1
 // @grant       GM.addStyle
 // ==/UserScript==
 
-function getElementFromURL(u, s) {
-  return fetch(u).then(r => r.text()).then(h => {
-    const p = new DOMParser()
-    const d = p.parseFromString(h, 'text/html')
-    return d.querySelector(s)
-  }).catch(e => console.warn('Extra Panel Userscript: Error occurred while retrieving panel.', e))
-}
+const pref = 'Extra Panel Userscript:'
+const getElementFromURL = (u, s, m = 'Error retrieving element.') => fetch(u).then(r => r.text()).then(h => (new DOMParser()).parseFromString(h, 'text/html')?.querySelector(s)).catch(e => console.warn(m, e) && null)
 
 async function main() {
   const bonusURL = document.querySelector('#extrapanelbutton a')?.href
-  if(!bonusURL) return console.log('Extra Panel Userscript: Button directing to Extra Panel not found.')
-  const bonusImage = await getElementFromURL(bonusURL, '.extrapanelimage') ?? document.createTextNode('Extra Panel Userscript: Extra Panel image not found.')
-  const container = document.querySelector('#comic img').parentNode
+  if (!bonusURL) return console.log(`${pref} Button directing to Extra Panel not found.`)
+  const bonusImage = await getElementFromURL(bonusURL, '.extrapanelimage', pref) ?? document.createTextNode(`${pref} Extra Panel image not found.`)
+  const container = document.querySelector('#comic img')?.parentNode ?? document.body
   GM.addStyle('.extrapanelimage { opacity: 0.1 } .extrapanelimage:hover { opacity: 1 }');
   container.appendChild(bonusImage)
 }
 
 main()
-
-/*function getElementFromURL(u, s) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.onload = function () {
-      if(this.status >= 200 && this.status < 300) resolve(xhr.response.querySelector(s))
-      else reject({ status: this.status, statusText: xhr.statusText })
-    }
-    xhr.onerror = function () {
-      reject({ status: this.status, statusText: xhr.statusText })
-    }
-    xhr.open('GET', u)
-    xhr.responseType = 'document'
-    xhr.send()
-  })
-}*/
